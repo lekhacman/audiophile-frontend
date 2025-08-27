c5bb4d16dd73f546e46a564713945b64e313f04f
andrew <andrew.le@accenture.com>
2025-08-27 23:31:19 +0800

    setup

---
+++ src/router.js
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router";
+import HomePage from "./module/home/HomePage.jsx";
+
+const router = createBrowserRouter([{ path: "/", Component: HomePage }]);
+
+export default router;
